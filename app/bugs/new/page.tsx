cbe6aeeca46bebf04fe649863422c7a0f9329233
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-02 21:01:04 +0200

    Form submition

--- app/bugs/new/page.tsx
+++ app/bugs/new/page.tsx
@@ -1,16 +1,35 @@
-"use client"
-import { Button, TextField } from '@radix-ui/themes'
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+
+interface bugForm {
+  title: string;
+  description: string;
+}
 
 export default function NewBugPage() {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<bugForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder='Otsikko' />
-        </TextField.Root>
-        <SimpleMDE placeholder='Kuvaus' />
-        <Button>Lisää bugi</Button>
-    </div>
-  )
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
+      await axios.post("/api/bugs", data)
+      router.push("/bugs")
+    })}>
+      <TextField.Root>
+        <TextField.Input placeholder="Otsikko" {...register("title")}/>
+      </TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) =><SimpleMDE placeholder="Kuvaus" {...field}/>}
+      />
+      <Button>Lisää bugi</Button>
+    </form>
+  );
 }
