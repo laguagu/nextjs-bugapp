b7dc6455841a64cfa407baf6b952e67bf739b1e2
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-03 09:16:27 +0200

    Error handling

--- app/bugs/new/page.tsx
+++ app/bugs/new/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
-
+import { useState } from "react";
 
 interface bugForm {
   title: string;
@@ -15,21 +15,36 @@ interface bugForm {
 export default function NewBugPage() {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<bugForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
-      await axios.post("/api/bugs", data)
-      router.push("/bugs")
-    })}>
-      <TextField.Root>
-        <TextField.Input placeholder="Otsikko" {...register("title")}/>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) =><SimpleMDE placeholder="Kuvaus" {...field}/>}
-      />
-      <Button>Lisää bugi</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/bugs", data);
+            router.push("/bugs");
+          } catch (error) {
+            setError("Error occured");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Otsikko" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="Kuvaus" {...field} />}
+        />
+        <Button>Lisää bugi</Button>
+      </form>
+    </div>
   );
 }
