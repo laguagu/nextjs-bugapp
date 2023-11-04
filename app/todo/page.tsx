1ba78ee0de1699103b38f4a2d2e6a48dc66d431b
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-04 20:42:37 +0200

    Todo page for task

---
+++ app/todo/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Todopage() {
+  return (
+    <>
+      <header>
+        <h1>Daily todos</h1>
+      </header>
+      <ul>
+        <li>Todo1</li>
+        <li>Todo2</li>
+      </ul>
+    </>
+  );
+}
